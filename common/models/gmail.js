de5dac45173c9c837eb2b57d50916a1c2bf9d2cc
adrianf823 <adrianf823@gmail.com>
2020-06-04 00:52:25 +0200

    ff

--- common/models/gmail.js
+++ common/models/gmail.js
@@ -2,15 +2,20 @@
 
 module.exports = function(Gmail) {
     Gmail.sendEmail = function(dest,pass,cb) {
-        Gmail.app.models.Email.send({
+        Gmail.app.models.Gmail.send({
           to: dest,
           from: 'pruebaenviogm@gmail.com',
           subject: 'Contraseña DualApp',
           text: 'La contraseña que le a sido asignada es: '+pass,
-          html: 'my <em>html</em>'
+          html: 'La contraseña que le a sido asignada es: '+pass
         }, function(err, mail) {
           console.log('email sent!');
           cb(err);
         });
       }
+      Gmail.remoteMethod('sendEmail', {
+        accepts:[ {arg: 'dest', type:'string'},
+        {arg: 'pass', type:'string'}],
+        returns: {arg: 'resp', type: 'string'}
+  });
 };
